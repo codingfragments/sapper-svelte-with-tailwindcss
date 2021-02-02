16f5523e2cf436e7bb5f49e49a320e6aa5d04c5c
Stefan Marx <smarx@marxworld.net>
2021-02-02 08:19:53 +0100

    beautify

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,24 +1,26 @@
 <script lang="ts">
-	import Nav from '../components/Nav.svelte';
-	import GlobalStyle from "../components/GlobalStyle.svelte";
+  import Nav from "../components/Nav.svelte";
+  import GlobalStyle from "../components/GlobalStyle.svelte";
 
-	export let segment: string;
+  export let segment: string;
 </script>
 
-<style>
-	main {
-		position: relative;
-		max-width: 56em;
-		background-color: white;
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-</style>
-
-<Nav {segment}/>
+<Nav {segment} />
 
 <main>
-	<GlobalStyle />
-	<slot></slot>
+  <GlobalStyle />
+  <slot />
 </main>
+
+<h1>ss</h1>
+
+<style>
+  main {
+    position: relative;
+    max-width: 56em;
+    background-color: white;
+    padding: 2em;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
+</style>
