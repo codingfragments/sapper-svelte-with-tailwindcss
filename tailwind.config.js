f49e9b4d4cc0c30d051a3602d83b88853441a545
Stefan Marx <smarx@marxworld.net>
2021-02-02 12:17:28 +0100

    default fonts explicit + adding materialui icons and roboton

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,6 +32,7 @@ module.exports = {
         secondaryDark: '#000',
       },
       black: colors.black,
+
       white: colors.white,
       gray: colors.coolGray,
       red: colors.red,
@@ -41,6 +42,11 @@ module.exports = {
       indigo: colors.indigo,
       purple: colors.violet,
       pink: colors.pink,
+    },
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif', 'ui-sans-serif'],
+      serif: ['ui-serif', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
+      mono: ['Menlo', 'ui-monospace', 'SFMono-Regular', 'Courier\ New', 'Monaco', 'monospace'],
     },
     extend: {},
   },
